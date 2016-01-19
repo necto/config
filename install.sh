aede3085a196ee7cd08514b42891fd8191492f65
Necto <necto.ne@gmail.com>
2016-01-19 15:39:37 +0100

    now you can call e-downloader from the install script

--- install.sh
+++ install.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 # Note bash is required for pushd/popd commands
 cfgdir=$(pwd)
+
+Y_opt=""
+Emerge_emacs=false
+while test $# -gt 0
+do
+    case "$1" in
+        -y | --yes) Y_opt="-y"
+            ;;
+        --emacs) Emerge_emacs=true
+                 ;;
+        *) echo "unexpected argument $1"
+           exit 1
+           ;;
+    esac
+    shift
+done
+
 echo "Configuration files are in $cfgdir"
 pushd ~
 echo "Deleting .emacs to replace it with .spacemacs"
@@ -17,3 +34,7 @@ fi
 echo "Downloading Spacemacs"
 git clone https://github.com/syl20bnr/spacemacs .emacs.d
 popd
+if [ Emerge_emacs -eq true]
+then
+    so ./emerge-emacs-snapshot-ubuntu.sh $Y_opt
+fi
