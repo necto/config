2e024168a6701410f1c0ec9069c975b0de567b9b
Necto <necto.ne@gmail.com>
2016-01-19 15:35:34 +0100

    no-ask option for emacs-snapshot-downloader

--- emerge-emacs-snapshot-ubuntu.sh
+++ emerge-emacs-snapshot-ubuntu.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-apt-get install software-properties-common
-add-apt-repository -y ppa:ubuntu-elisp
+Y_opt=""
+while test $# -gt 0
+do
+    case "$1" in
+        -y | --no-ask | --auto | --yes) Y_opt="-y"
+            ;;
+        *) echo "unexpected argument $1"
+           exit 1
+           ;;
+    esac
+    shift
+done
+apt-get install $Y_opt software-properties-common
+add-apt-repository $Y_opt ppa:ubuntu-elisp
 apt-get update
-apt-get install emacs-snapshot
+apt-get install $Y_opt emacs-snapshot
+
