8c85685523a053ee2a464c09d9027f8d99844b5e
Necto <necto.ne@gmail.com>
2016-07-11 18:29:27 +0200

    DotBot install config replacing the old shell script

---
+++ emerge-cabal.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+## Useful haskell packets
+sudo apt install cabal-install
+cabal update
+cabal install stylish-haskell
+cabal install hlint
+cabal install hasktags
+cabal install ghc-mod
