37ae2e22a7273cdfb747e9946d2ea7dd8f11342f
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2020-03-25 17:54:40 +0100

    Add the screenlock after suspend hook to fix the ru-pass issue
    I.e. when you leave a russian keyboard layout before the machine
    falls asleep, you are locked out when it wakes up,
    because there is no way to change the layout in the screen locker.
    
    Fixed by disabling the GDM screen lock, and using the
    systemd hook that runs slock (with setxkbmap us before that).

--- xmonad/switch_keyboard_layout.sh
+++ xmonad/switch_keyboard_layout.sh
@@ -2,7 +2,7 @@
 
 LANG1="us"
 LANG2="ru"
-CURRENT_LANG=$(setxkbmap -query | tail -n 1 | cut -f6 -d ' ')
+CURRENT_LANG=$(setxkbmap -query | grep 'layout' | cut -f6 -d ' ')
 if [ "$CURRENT_LANG" = $LANG1 ]; then
     setxkbmap $LANG2
 else
