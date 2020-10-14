aca8fbc442c314035358bd2954bed0d7cccdde15
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2020-10-14 18:29:40 +0200

    advance the sonar-source env setup script

--- sonar-source-environment.sh
+++ sonar-source-environment.sh
@@ -1,5 +1,6 @@
 # SonarSource cpecific:
-apt install -y zlib1g-dev
+apt install -y zlib1g-dev python3-pip
+pip3 install lit
 
 # download llvm fork from https://github.com/SonarSource/llvm-project/releases put it into ~/proj/sonarsource-clang*
 
@@ -14,11 +15,15 @@ CC=clang-10 CXX=clang++-10 cmake -DUSE_CCACHE=ON -DCMAKE_EXPORT_COMPILE_COMMANDS
 ninja
 cd ..
 mkdir -p exe/tester
-ln -s cmake/tester exe/tester/tester
+ln -s ../../cmake/tester exe/tester/tester
 mkdir -p exe/reproducer
-ln -s cmake/reproducer exe/reproducer/reproducer
+ln -s ../../cmake/reproducer exe/reproducer/reproducer
 mkdir -p exe/subprocess
-ln -s cmake/subprocess exe/subprocess/subprocess
+ln -s ../../cmake/subprocess exe/subprocess/subprocess
 cd ..
 ln -s build/cmake/compile_commands.json ./
 ln -s ~/config/.clang-format ./
+
+# Gradle stuff
+apt install -y default-jdk
+# to be continued...
