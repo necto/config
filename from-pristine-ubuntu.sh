c9d1300940fa0a650a8609e9b5236b695f70d863
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2020-10-14 15:28:49 +0200

    An attempt to call emerge-spacemacs automatically

--- from-pristine-ubuntu.sh
+++ from-pristine-ubuntu.sh
@@ -1,3 +1,7 @@
+
+# Docker container to try it out:
+# docker run -it -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --user="$(id --user):$(id --group)" ubuntu bash
+
 apt update
 apt install -y git python emacs xmonad
 # emacs package asks for geographic area: 8.Europe 63.Zurich
