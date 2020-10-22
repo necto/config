dfe1be5602bcb7c204f2c0ead1af5a36441c2e49
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2020-10-22 18:42:58 +0200

    refactor the forward Dropbox folder script

--- forward-dropbox-dir.sh
+++ forward-dropbox-dir.sh
@@ -1,4 +1,6 @@
+ORIG_USER=necto
+FWD_USER=arseniy
 sudo apt install bindfs
-sudo mkdir -p /home/arseniy/Dropbox
-sudo chown -R arseniy:arseniy /home/arseniy/Dropbox
-sudo bindfs --map=necto/arseniy /home/necto/Dropbox/ /home/arseniy/Dropbox
+sudo mkdir -p /home/$ORIG_USER/Dropbox
+sudo chown -R $FWD_USER:$FWD_USER /home/$FWD_USER/Dropbox
+sudo bindfs --map=$ORIG_USER/$FWD_USER /home/$ORIG_USER/Dropbox/ /home/$FWD_USER/Dropbox
