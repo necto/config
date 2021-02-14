d49c48956167c1a42052c2903c8a28a18ccedfe3
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2021-02-14 18:11:04 +0100

    Add syncthing

--- from-pristine-ubuntu.sh
+++ from-pristine-ubuntu.sh
@@ -24,6 +24,17 @@ echo "X------------------------X"
 # Dev packages
 sudo apt install -y cmake ninja-build clang-10 ccache gcc lld clangd
 
+#install syncthing
+sudo curl -s -o /usr/share/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
+printf "Package: *\nPin: origin apt.syncthing.net\nPin-Priority: 990\n" | sudo tee /etc/apt/preferences.d/syncthing
+sudo apt-get update
+sudo apt install -y syncthing
+systemctl --user enable syncthing.service
+systemctl --user start syncthing.service
+
+# Now go to the web interface localhost:8384 and setup the paired devices and shared folders
+
 # TODO: configure firefox, gtk (font size, etc), xmonad-as-the-default, docker
 
 # ln -s ~/.xmonad/bin/xsession ~/.xsession
@@ -35,3 +46,4 @@ sudo apt install -y cmake ninja-build clang-10 ccache gcc lld clangd
 
 # Hide the menu-bar in the gnome terminal by default:
 # gsettings set org.gnome.Terminal.Legacy.Settings default-show-menubar false
+
