6cb3254fa8bf2045f2d211ead534fe7155f9a42f
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2023-10-03 15:29:00 +0200

    Modernize work bootstrap script

--- sonar-source-environment.sh
+++ sonar-source-environment.sh
@@ -1,27 +1,81 @@
 # SonarSource cpecific:
 # this list partially replicates "from-pristine-ubuntu.sh", for the self-sufficiency
-sudo apt install -y zlib1g-dev python3-pip ninja-build clangd-13 clang-13 cmake ccache gcc lld
-pip3 install lit==12.0.1
+sudo apt install -y zlib1g-dev python3-pip ninja-build clangd-17 clang-17 cmake ccache gcc lld
+pip3 install lit==15.0.7
 
-# download llvm fork from https://github.com/SonarSource/llvm-project/releases put it into ~/sonarsource-clang*
-
+# Checkout our fork of llvm
 mkdir -p ~/proj
 cd ~/proj
-mv ~/sonarsource-clang* ./
+git clone gh:SonarSource/llvm-project
+cd llvm-project
 
-tar -xvf sonarsource-clang*
+# Checkout Z3 submodule
+git submodule init
+git submodule update
 
+# Build Z3
+mkdir z3-build
+cd z3-build
+CC=clang-17 CXX=clang++-17 cmake \
+  -DZ3_BUILD_LIBZ3_SHARED=OFF \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_INSTALL_PREFIX=../llvm-install \
+  -GNinja \
+  ../z3
+ninja
+ninja install
+
+# Build LLVM (takes a while)
+mkdir -p build
+echo 'CC=clang-17 CXX=clang++-17 cmake \
+        -DLLVM_Z3_INSTALL_DIR=${PWD}/../llvm-install \
+        -DLLVM_ENABLE_Z3_SOLVER=ON \
+        -DLLVM_ENABLE_TERMINFO=OFF \
+        -DLLVM_ENABLE_ZSTD=OFF \
+        -DCMAKE_BUILD_TYPE=assertions \
+        -DLLVM_ENABLE_ASSERTIONS=ON \
+        -DLLVM_ENABLE_DUMP=ON \
+        -DLLVM_TARGETS_TO_BUILD=X86 \
+        -DCMAKE_INSTALL_PREFIX=../llvm-install \
+        -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" \
+        -DCMAKE_CXX_STANDARD=17 \
+        -DLLVM_USE_LINKER=lld \
+        -GNinja \
+        ../llvm' > build/cmake.sh
+cd build
+bash cmake.sh
+ninja
+cd ~/proj
+
+# Add our forked clang-format to PATH
+mkdir -p ~/.sonar/bin/
+echo 'export PATH="~/.sonar/bin/:$PATH"' > ~/.profile
+ln -s ~/proj/llvm-project/build/bin/clang-format ~/.sonar/bin/
+
+# JNI is needed to build our project
+sudo apt install -y openjdk-19-jdk default-jdk
+
+# Checkout and build sonar-cpp using LLVM sources directly
+cd ~/proj
 git clone gh:SonarSource/sonar-cpp
 cd sonar-cpp
 mkdir -p build/asserts
-echo 'CC=clang-13 CXX=clang++-13 cmake -DUSE_CCACHE=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DUSE_ASSERTIONS=ON -DUSE_PCH=OFF -DBUILD_SHARED_LIBS=ON -DUSE_LINKER=lld -GNinja ../..' > build/build-asserts.sh
+echo 'CC=clang-17 CXX=clang++-17 \
+    cmake -DUSE_CCACHE=ON \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DUSE_LLVM_SOURCE=../../../llvm-project/ \
+    -DUSE_ASSERTIONS=ON \
+    -DUSE_PCH=OFF \
+    -DBUILD_SHARED_LIBS=ON \
+    -DUSE_LINKER=lld \
+    -GNinja \
+    ../..' > build/cmake-asserts.sh
 cd build/asserts
-bash ../build-asserts.sh
+bash ../cmake-asserts.sh
 ninja
 cd ../..
 ln -s build/asserts/compile_commands.json ./
-ln -s ~/config/.clang-format ./
 
 # Gradle stuff
-# sudo apt install -y default-jdk
 # to be continued...
