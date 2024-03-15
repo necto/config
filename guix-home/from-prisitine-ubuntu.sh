a1cc8057c069ff99fe387c15df6b0ea642edfedd
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2024-03-15 16:22:05 +0100

    more accurate bootstrap script

--- guix-home/from-prisitine-ubuntu.sh
+++ guix-home/from-prisitine-ubuntu.sh
@@ -3,12 +3,16 @@ sudo apt install -y xmonad guix git
 
 # takes long time and can take advantage of many cores
 guix pull
-guix pull # just to be sure
+export GUIX_PROFILE="$HOME/.config/guix/current"
+. "$GUIX_PROFILE/etc/profile"
+guix pull
 
 git clone https://github.com/necto/config
 
 # takes long time and can take advantage of many cores
-# FIXME: fails at the end for some reason
+# FIXME: fails at the end if I uncomment doom-checkout service
+# the substitute* behaves differently when it is invoked from a file loaded from /gnu/store
+# and from a file loaded from another place
 # ice-9/boot-9.scm:1685:16: In procedure raise-exception:
 # Wrong type to apply: "/usr/bin/env sh"
 guix home reconfigure config/guix-home/home-configuration.scm
