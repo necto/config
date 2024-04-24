4bc7c7cf0dc4ae1c8166d390d0c434037903c474
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2024-04-24 05:32:42 +0200

    Building llvm with zlib enabled

--- guix-home/llvm-build-sample-cmake.sh
+++ guix-home/llvm-build-sample-cmake.sh
@@ -1,6 +1,10 @@
-CC=clang-18 CXX=clang++ cmake \
+# -DCMAKE_BUILD_RPATH=/home/necto/.guix-profile/lib
+# is necessary because libz.so is not available in the default search path
+# even though cmake finds it properly, llvm cmake does not
+# add the matching RPATH and invocations of tblgen fail during
+# compilation
+CC=clang CXX=clang++ cmake \
         -DLLVM_ENABLE_TERMINFO=OFF \
-        -DLLVM_ENABLE_ZLIB=OFF \
         -DLLVM_ENABLE_ZSTD=OFF \
         -DCMAKE_BUILD_TYPE=Debug \
         -DLLVM_ENABLE_ASSERTIONS=ON \
@@ -10,5 +14,6 @@ CC=clang-18 CXX=clang++ cmake \
         -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra;lld" \
         -DCMAKE_CXX_STANDARD=17 \
         -DLLVM_USE_LINKER=lld \
+        -DCMAKE_BUILD_RPATH=/home/necto/.guix-profile/lib \
         -GNinja \
         ../llvm
