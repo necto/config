036b40b3214c4fdd431af8a32fa2fbdd1af43165
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2024-06-08 10:06:18 +0200

    Fix paths in the after-reboot script

--- guix-home/after-reboot.sh
+++ guix-home/after-reboot.sh
@@ -6,7 +6,7 @@ set -xeuo pipefail
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # install the systemd and xdg-desktop-portal configs for xdg-desktop-portal-wlr
-sudo bash ".config/xdg-desktop-portal-wlr-install.sh"
+sudo bash "$HOME/.config/xdg-desktop-portal-wlr-install.sh"
 
 # Refresh all channels, including nonguix that was added by guix-home
 # --fallback will make guix build the missing package from source if substitute fails
@@ -18,8 +18,14 @@ guix package -m "$SCRIPT_DIR/manifest.scm"
 # Install doom
 bash "$HOME/.config/doom/install.sh"
 
-echo "Go to the Bitwarden page to install it interactively"
-$HOME/.guix-home/bin/firefox https://addons.mozilla.org/en-US/firefox/addon/bitwarden-password-manager/
+echo "[ ] Go to the Bitwarden page to install it interactively"
+$HOME/.guix-profile/bin/firefox https://addons.mozilla.org/en-US/firefox/addon/bitwarden-password-manager/
 
-echo "Go to the Syncthing page to configure the peers"
-$HOME/.guix-home/bin/firefox https://localhost:8384
+echo "[ ] log-in to bitwarden"
+echo "[ ] set auto-lock for 5 minutes"
+
+echo "[ ] Go to the Syncthing page to configure the peers"
+$HOME/.guix-profile/bin/firefox https://localhost:8384
+
+echo "[ ] setup wifi"
+echo "[ ] add key to github"
