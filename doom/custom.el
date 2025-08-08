15617e20721009808ffc2df7fdb7734627eb85b1
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2025-08-08 14:45:24 +0200

    Admit safe dirlocals for clean.csv

--- doom/custom.el
+++ doom/custom.el
@@ -4,67 +4,61 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("51c71bb27bdab69b505d9bf71c99864051b37ac3de531d91fdad1598ad247138" "1a1ac598737d0fcdc4dfab3af3d6f46ab2d5048b8e72bc22f50271fd6d393a00" "02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" default))
+   '("51c71bb27bdab69b505d9bf71c99864051b37ac3de531d91fdad1598ad247138"
+     "1a1ac598737d0fcdc4dfab3af3d6f46ab2d5048b8e72bc22f50271fd6d393a00"
+     "02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" default))
  '(safe-local-variable-values
-   '((eval with-eval-after-load 'git-commit
+   '((eval when
+      (string-equal (file-name-nondirectory buffer-file-name) "clean.csv")
+      (nutri-journal-mode))
+     (eval with-eval-after-load 'git-commit
       (add-to-list 'git-commit-trailers "Change-Id"))
-     (eval progn
-      (require 'lisp-mode)
-      (defun emacs27-lisp-fill-paragraph
-          (&optional justify)
+     (eval progn (require 'lisp-mode)
+      (defun emacs27-lisp-fill-paragraph (&optional justify)
         (interactive "P")
-        (or
-         (fill-comment-paragraph justify)
-         (let
-             ((paragraph-start
-               (concat paragraph-start "\\|\\s-*\\([(;\"]\\|\\s-:\\|`(\\|#'(\\)"))
-              (paragraph-separate
-               (concat paragraph-separate "\\|\\s-*\".*[,\\.]$"))
-              (fill-column
-               (if
-                   (and
-                    (integerp emacs-lisp-docstring-fill-column)
-                    (derived-mode-p 'emacs-lisp-mode))
-                   emacs-lisp-docstring-fill-column fill-column)))
-           (fill-paragraph justify))
-         t))
+        (or (fill-comment-paragraph justify)
+            (let
+                ((paragraph-start
+                  (concat paragraph-start
+                          "\\|\\s-*\\([(;\"]\\|\\s-:\\|`(\\|#'(\\)"))
+                 (paragraph-separate
+                  (concat paragraph-separate "\\|\\s-*\".*[,\\.]$"))
+                 (fill-column
+                  (if
+                      (and (integerp emacs-lisp-docstring-fill-column)
+                           (derived-mode-p 'emacs-lisp-mode))
+                      emacs-lisp-docstring-fill-column
+                    fill-column)))
+              (fill-paragraph justify))
+            t))
       (setq-local fill-paragraph-function #'emacs27-lisp-fill-paragraph))
-     (eval modify-syntax-entry 43 "'")
-     (eval modify-syntax-entry 36 "'")
-     (eval modify-syntax-entry 126 "'")
-     (geiser-repl-per-project-p . t)
+     (eval modify-syntax-entry 43 "'") (eval modify-syntax-entry 36 "'")
+     (eval modify-syntax-entry 126 "'") (geiser-repl-per-project-p . t)
      (eval with-eval-after-load 'yasnippet
       (let
           ((guix-yasnippets
             (expand-file-name "etc/snippets/yas"
-                              (locate-dominating-file default-directory ".dir-locals.el"))))
-        (unless
-            (member guix-yasnippets yas-snippet-dirs)
-          (add-to-list 'yas-snippet-dirs guix-yasnippets)
-          (yas-reload-all))))
+                              (locate-dominating-file default-directory
+                                                      ".dir-locals.el"))))
+        (unless (member guix-yasnippets yas-snippet-dirs)
+          (add-to-list 'yas-snippet-dirs guix-yasnippets) (yas-reload-all))))
      (eval with-eval-after-load 'tempel
-      (if
-          (stringp tempel-path)
-          (setq tempel-path
-                (list tempel-path)))
+      (if (stringp tempel-path) (setq tempel-path (list tempel-path)))
       (let
           ((guix-tempel-snippets
             (concat
              (expand-file-name "etc/snippets/tempel"
-                               (locate-dominating-file default-directory ".dir-locals.el"))
+                               (locate-dominating-file default-directory
+                                                       ".dir-locals.el"))
              "/*.eld")))
-        (unless
-            (member guix-tempel-snippets tempel-path)
+        (unless (member guix-tempel-snippets tempel-path)
           (add-to-list 'tempel-path guix-tempel-snippets))))
      (eval setq-local guix-directory
       (locate-dominating-file default-directory ".dir-locals.el"))
      (eval add-to-list 'completion-ignored-extensions ".go")))
  '(warning-suppress-types
-   '((copilot copilot-no-mode-indent)
-     (copilot copilot-no-mode-ident)
-     (copilot copilot-exceeds-max-char)
-     (defvaralias)
-     (lexical-binding))))
+   '((copilot copilot-no-mode-indent) (copilot copilot-no-mode-ident)
+     (copilot copilot-exceeds-max-char) (defvaralias) (lexical-binding))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
