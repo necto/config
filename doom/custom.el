3a20dc9fd8a9c358b126a689bed8c33a27fdc14b
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2025-08-16 15:40:07 +0200

    consider safe directories: doom, nutri-journal, emacs

--- doom/custom.el
+++ doom/custom.el
@@ -7,6 +7,9 @@
    '("51c71bb27bdab69b505d9bf71c99864051b37ac3de531d91fdad1598ad247138"
      "1a1ac598737d0fcdc4dfab3af3d6f46ab2d5048b8e72bc22f50271fd6d393a00"
      "02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" default))
+ '(safe-local-variable-directories
+   '("/home/necto/proj/nutri-journal/journals/" "/home/necto/.config/doom/"
+     "/home/necto/.config/emacs/"))
  '(safe-local-variable-values
    '((eval when
       (string-equal (file-name-nondirectory buffer-file-name) "clean.csv")
