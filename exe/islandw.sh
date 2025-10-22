27bae6649a5583a7adc6b50e2f9f6a38cf11575c
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2025-10-22 09:36:01 +0200

    Replace custom wayland-nudging wrapper with an environment variable

--- exe/islandw.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Run Slack in Wayland-mode (electron option)
-# --enable-features=WaylandWindowDecorations seems to be unnecessary
-# Slack seems to ignore the ~/.config/electron25-flags.conf file
-# so passing it as in a cmdline instead
-island-browser --ozone-platform-hint=auto "$@"
--- exe/slackw.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Run Slack in Wayland-mode (electron option)
-# --enable-features=WaylandWindowDecorations seems to be unnecessary
-# Slack seems to ignore the ~/.config/electron25-flags.conf file
-# so passing it as in a cmdline instead
-
-slack --ozone-platform-hint=auto
